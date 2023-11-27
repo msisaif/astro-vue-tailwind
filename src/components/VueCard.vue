c055a0078ed77a97a48136ac2ef8b9b32647903a
msisaif <msi132043@gmail.com>
2023-11-27 08:19:17 +0600

    replace custom css by tailwind css

---
+++ src/components/VueCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  body: String,
+  href: String,
+});
+
+const { href, title, body } = props;
+</script>
+
+<template>
+  <div class="bg-gray-600 text-white p-4 rounded-lg">
+    <a :href="href" target="_blank">
+      <h2>
+        {{ title }}
+        <span>&rarr;</span>
+      </h2>
+      <p>{{ body }}</p>
+    </a>
+  </div>
+</template>
