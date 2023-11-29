ff8de25761b030fbeff80e06ae9b53a286713cae
msisaif <msi132043@gmail.com>
2023-11-29 09:21:27 +0600

    formate by prettier

--- src/components/VueCard.vue
+++ src/components/VueCard.vue
@@ -1,21 +1,21 @@
 <script setup>
 const props = defineProps({
-  title: String,
-  body: String,
-  href: String,
-});
+	title: String,
+	body: String,
+	href: String
+})
 
-const { href, title, body } = props;
+const { href, title, body } = props
 </script>
 
 <template>
-  <div class="bg-gray-600 text-white p-4 rounded-lg">
-    <a :href="href" target="_blank">
-      <h2>
-        {{ title }}
-        <span>&rarr;</span>
-      </h2>
-      <p>{{ body }}</p>
-    </a>
-  </div>
+	<div class="rounded-lg bg-gray-600 p-4 text-white">
+		<a :href="href" target="_blank">
+			<h2>
+				{{ title }}
+				<span>&rarr;</span>
+			</h2>
+			<p>{{ body }}</p>
+		</a>
+	</div>
 </template>
